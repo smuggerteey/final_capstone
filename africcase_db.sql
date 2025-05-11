ae1d6c55eaa1a0fb8af0d55f2b87de8734dc7064
smuggerteey <tynochagaka@gmail.com>
2025-05-11 18:18:23 +0000

    Moving files

--- africcase_db.sql
+++ africcase_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 28, 2025 at 12:53 PM
+-- Generation Time: May 11, 2025 at 08:17 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
